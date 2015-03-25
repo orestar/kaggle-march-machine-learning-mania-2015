25a6efe559b9dfeedf227737f29669ad9dc6eaaa
orestar <aurelienbertho@yahoo.fr>
2015-03-25 14:23:14 -0400

    Update heavy_seed_weighting.R

--- heavy_seed_weighting.R
+++ heavy_seed_weighting.R
@@ -1,17 +1,20 @@
-# Load Data
+# Load Data matches
 matches <- read.csv("Data/sample_submission.csv", header = TRUE, stringsAsFactors = FALSE)
+# Load Data seeds
 seeds <- read.csv("Data/tourney_seeds.csv", header = TRUE, stringsAsFactors = FALSE)
 
 matches$year <- sapply(matches$id, FUN=function(x) {strsplit(x, split='[_]')[[1]][1]})
 matches$lowId <- sapply(matches$id, FUN=function(x) {strsplit(x, split='[_]')[[1]][2]})
 matches$highId <- sapply(matches$id, FUN=function(x) {strsplit(x, split='[_]')[[1]][3]})
 
+# Seeds
 seeds <- subset(seeds, season >= 2011)
 seeds$seedNumber <- as.numeric(substring(seeds$seed, 2, 3))
 
 matches <- cbind(matches, merge(matches, seeds, by.x=c("year","lowId"), by.y=c("season","team"))["seedNumber"])
 colnames(matches)[6] <- "lowIdSeed"
 
+# Test
 test <- merge(matches, seeds, by.x=c("year","highId"), by.y=c("season","team"))
 test <- test[order(test$id), ]
 
@@ -20,5 +23,7 @@ colnames(matches)[7] <- "highIdSeed"
 rownames(matches) <- NULL
 
 matches$calculated <- 0.50 + (matches$highIdSeed - matches$lowIdSeed)*0.03333333333
+# Submit Data 
 submit <- data.frame(id = matches$id, pred = matches$calculated)
+# Write heavy_simple_seed.csv
 write.csv(submit, file = "Output/heavy_simple_seed.csv", row.names = FALSE, quote=FALSE)
