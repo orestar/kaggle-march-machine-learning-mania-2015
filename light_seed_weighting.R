533a3e3ef27c4bc2a3b04025c008dd96df167676
orestar <aurelienbertho@yahoo.fr>
2015-03-25 14:24:26 -0400

    Update light_seed_weighting.R

--- light_seed_weighting.R
+++ light_seed_weighting.R
@@ -1,5 +1,6 @@
-# Load Data
+# Load Data matches
 matches <- read.csv("Data/sample_submission.csv", header = TRUE, stringsAsFactors = FALSE)
+# Load Data seeds
 seeds <- read.csv("Data/tourney_seeds.csv", header = TRUE, stringsAsFactors = FALSE)
 
 matches$year <- sapply(matches$id, FUN=function(x) {strsplit(x, split='[_]')[[1]][1]})
@@ -20,5 +21,7 @@ colnames(matches)[7] <- "highIdSeed"
 rownames(matches) <- NULL
 
 matches$calculated <- 0.50 + (matches$highIdSeed - matches$lowIdSeed)*0.0295
+# Submit data
 submit <- data.frame(id = matches$id, pred = matches$calculated)
+# Write file
 write.csv(submit, file = "Output/light_simple_seed_295.csv", row.names = FALSE, quote=FALSE)
